1f9fc42333e9f813a71e42ef59b7bf29353e44f8
unknown <hanadi.hosri@gmail.com>
2024-03-11 21:57:54 +0200

    adding evaluate-operations file

---
+++ evaluate-operations.php
@@ -0,0 +1,8 @@
+<?php
+// data types
+$string = "Hello from PHP";
+$integer = 42;
+$float = 3.14;
+$boolean = true;
+
+echo gettype($string) . $string  . "\n" . gettype($integer) . $integer . "\n" . gettype($float) . $float . "\n" . gettype($boolean) . $boolean;
