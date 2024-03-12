e8ece072d881f9432598ce85c65da0564da82b57
unknown <hanadi.hosri@gmail.com>
2024-03-12 08:32:56 +0200

    adding first part of the validateIpv4 function

--- validate-ipv4.php
+++ validate-ipv4.php
@@ -1,1 +1,27 @@
 <?php
+function validateIpv4 ($ip) {
+    $numbers = [];
+    $dots = [];
+    $currentNum = "";
+    for($i = 0; $i < strlen($ip); $i++) {
+        if (substr($ip, $i, 1) == "." && substr($ip, $i+1, 1) == ".") {
+            echo "not a valid IPv4";
+            break;
+        }
+        if (substr($ip, $i, 1) == ".") {
+            array_push($dots, ".");
+        }
+        if (is_numeric(substr($ipn, $i, 1))) {
+            $currentNum += substr($ip, $i, 1);
+        } elseif (substr($ipn, $i, 1) == ".") {
+            array_push($numbers, $currentNum);
+            $currentNum = "";
+        }
+    }
+    if (count($dots) != 3) {
+        echo "not a valid IPv4";
+    }
+    if (count($numbers) != 4) {
+        echo "not a valid IPv4";
+    } 
+}
