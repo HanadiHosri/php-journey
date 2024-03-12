6021c9980ab20ce6af3de94167579b5272fdef51
unknown <hanadi.hosri@gmail.com>
2024-03-12 09:17:37 +0200

    finalizing validateIpv4

--- validate-ipv4.php
+++ validate-ipv4.php
@@ -1,27 +1,37 @@
 <?php
-function validateIpv4 ($ip) {
+function validateIpv4($ip) {
     $numbers = [];
     $dots = [];
     $currentNum = "";
     for($i = 0; $i < strlen($ip); $i++) {
         if (substr($ip, $i, 1) == "." && substr($ip, $i+1, 1) == ".") {
-            echo "not a valid IPv4";
-            break;
+            return "not a valid IPv4";
         }
         if (substr($ip, $i, 1) == ".") {
             array_push($dots, ".");
         }
-        if (is_numeric(substr($ipn, $i, 1))) {
-            $currentNum += substr($ip, $i, 1);
-        } elseif (substr($ipn, $i, 1) == ".") {
+        if (is_numeric(substr($ip, $i, 1))) {
+            $currentNum = $currentNum . substr($ip, $i, 1);
+        } elseif (substr($ip, $i, 1) == ".") {
             array_push($numbers, $currentNum);
             $currentNum = "";
         }
     }
+    array_push($numbers, $currentNum);
     if (count($dots) != 3) {
-        echo "not a valid IPv4";
+        return "not a valid IPv4";
     }
     if (count($numbers) != 4) {
-        echo "not a valid IPv4";
-    } 
+        return "not a valid IPv4";
+    } else {
+        for ($i = 0; $i < 4; $i++) {
+            if ((int)$numbers[$i] < 0 || (int)$numbers[$i] > 255) {
+                return "not a valid IPv4";
+            }
+        }
+        return "valid IPv4";
+    }
+
 }
+
+echo validateIpv4("100.2.300.3");
