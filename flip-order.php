644b24a712445282234aa7a9367e75f86a2a0d4b
unknown <hanadi.hosri@gmail.com>
2024-03-12 06:53:04 +0200

    implementing the flipOrder function

--- flip-order.php
+++ flip-order.php
@@ -1,1 +1,11 @@
 <?php
+function flipOrder($num) {
+    if ($num < 10) {
+        return $num;
+    } else {
+        $numlength = strlen((string)$num);
+        return ($num % 10) * ((10)**($numlength-1)) + flipOrder((int)($num/10));
+    }
+}
+
+echo flipOrder(1234);
